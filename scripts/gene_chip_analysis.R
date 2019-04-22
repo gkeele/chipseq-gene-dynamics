15fe5aacf5d06f70f181433dc5a09ca43b45b35d
Greg Keele <gkeele@gmail.com>
2019-04-21 21:34:42 -0400

    Update

--- scripts/gene_chip_analysis.R
+++ scripts/gene_chip_analysis.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(brms)
 
 setwd("~/projects/chipseq-gene-dynamics/")
+#setwd("~/Documents/git_repositories/chipseq-gene-dynamics/")
+
 
 ## Read in the data
 gene_chip_dat <- read.table("data/scaled_matrix_for_LMM_model_nonOverlappingGenes.txt") # path to data
@@ -105,10 +107,15 @@ yal041w_models <- run_brms_on_chipseq(chipseq_dat = gene_chip_dat, this_gene = "
 
 ## Parsing full set of Stan results
 stan_fit_paths <- list.files("individual_gene_Stan_models/", full.names = TRUE)
+#stan_fit_paths <- list.files("individual_gene_Stan_models_alpha_0.99_iter_5000//", full.names = TRUE)
+#file_path <- "individual_gene_Stan_models//sacCer3_nonOverlappingGenes_noChrMGenes_"
+file_path <- "individual_gene_Stan_models_alpha_0.99_iter_5000///sacCer3_nonOverlappingGenes_noChrMGenes_"
 for (i in 1:length(stan_fit_paths)) {
   this_fit <- readRDS(stan_fit_paths[i])
-  this_gene <- gsub(x = stan_fit_paths[i], pattern = "individual_gene_Stan_models//sacCer3_nonOverlappingGenes_noChrMGenes_", replacement = "", fixed = TRUE) %>%
+
+  this_gene <- gsub(x = stan_fit_paths[i], pattern = file_path, replacement = "", fixed = TRUE) %>%
     gsub(x = ., replacement = "", pattern = "_Stan.rds", fixed = TRUE)
+  
   
   if (i == 1) {
     timepoint_dat <- bind_rows(data.frame(gene = this_gene, assay = "writer_loss", this_fit$writer_loss %>% filter(parameter == "timepoint")),
@@ -124,9 +131,8 @@ for (i in 1:length(stan_fit_paths)) {
 }
 timepoint_dat <- timepoint_dat %>% 
   select(-parameter) %>%
-  mutate(estimate = estimate * 60,
-         is_pos = ifelse(lower_95 > 0), TRUE, FALSE,
-         is_neg = ifelse(upper_95 < 0), TRUE, FALSE)
+  mutate(is_pos = ifelse(lower_95 > 0, TRUE, FALSE),
+         is_neg = ifelse(upper_95 < 0, TRUE, FALSE))
 timepoint_dat$category[timepoint_dat$lower_95 > 0] <- "positive"
 timepoint_dat$category[timepoint_dat$upper_95 < 0] <- "negative"
 timepoint_dat$category[timepoint_dat$lower_95 < 0 & timepoint_dat$upper_95 > 0] <- "zero"
@@ -218,13 +224,27 @@ plot(gene_timepoint_dat$writer_add, gene_timepoint_dat$writer_loss, xlim = c(-5,
 abline(h = 0, lty = 2)
 abline(v = 0, lty = 2)
 
+
+## Looking at differences between wl and wel
+practice_dat <- gene_chip_dat %>% 
+  filter(gene == gene_chip_dat$gene[10],
+         value != 0 & value != 1,
+         assay %in% c("writer_loss", "writer_eraser_loss"))
+compare_fit <- brms::brm(value ~ 1 + timepoint + assay + timepoint:assay + (1 + timepoint + assay + timepoint:assay | sample_unit), 
+                    data = practice_dat, 
+                    family = "beta",
+                    iter = 2000,
+                    control = list(adapt_delta = 0.8))
+
+
+
 ## Assay specific plots
 mean_gene_chip_dat <- gene_chip_dat %>%
-  group_by(gene, assay, timepoint, batch_id, rep_id) %>%
+  group_by(gene, assay, timepoint, sample_unit, rep_id) %>%
   summarise(value = mean(value)) %>%
   ungroup
 # Writer loss
-writer_loss <- ggplot(data = mean_gene_chip_dat %>% filter(assay == "writer_loss"), aes(x = timepoint, y = value, color = batch_id)) + geom_point() + geom_line()
+writer_loss <- ggplot(data = mean_gene_chip_dat %>% filter(assay == "writer_loss"), aes(x = timepoint, y = value, color = sample_unit)) + geom_point() + geom_line()
 writer_loss <- writer_loss + scale_color_grey()
 writer_loss <- writer_loss + geom_smooth(aes(y = value, x = timepoint), method = "lm", col = "magenta") + ggtitle("Writer loss")
 writer_loss <- writer_loss + theme(panel.grid.major = element_blank(), 
@@ -237,7 +257,7 @@ writer_loss <- writer_loss + theme(panel.grid.major = element_blank(),
                                    axis.text.x = element_text(hjust = 1, face = "bold")) + guides(color = FALSE)
 writer_loss
 # Writer and eraser loss
-writer_eraser_loss <- ggplot(data = mean_gene_chip_dat %>% filter(assay == "writer_eraser_loss"), aes(x = timepoint, y = value, color = batch_id)) + geom_point() + geom_line()
+writer_eraser_loss <- ggplot(data = mean_gene_chip_dat %>% filter(assay == "writer_eraser_loss"), aes(x = timepoint, y = value, color = sample_unit)) + geom_point() + geom_line()
 writer_eraser_loss <- writer_eraser_loss + scale_color_grey()
 writer_eraser_loss <- writer_eraser_loss + geom_smooth(aes(y = value, x = timepoint), method = "lm", col = "seagreen1") + ggtitle("Writer and eraser loss")
 writer_eraser_loss <- writer_eraser_loss + theme(panel.grid.major = element_blank(), 
@@ -250,7 +270,7 @@ writer_eraser_loss <- writer_eraser_loss + theme(panel.grid.major = element_blan
                                                  axis.text.x = element_text(hjust = 1, face = "bold")) + guides(color = FALSE)
 writer_eraser_loss
 # Writer add
-writer_add <- ggplot(data = mean_gene_chip_dat %>% filter(assay == "writer_add"), aes(x = timepoint, y = value, color = batch_id)) + geom_point() + geom_line()
+writer_add <- ggplot(data = mean_gene_chip_dat %>% filter(assay == "writer_add"), aes(x = timepoint, y = value, color = sample_unit)) + geom_point() + geom_line()
 writer_add <- writer_add + scale_color_grey()
 writer_add <- writer_add + geom_smooth(aes(y = value, x = timepoint), method = "lm", col = "coral") + ggtitle("Writer add")
 writer_add <- writer_add + theme(panel.grid.major = element_blank(), 
